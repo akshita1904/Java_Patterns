6bbff334c7d41a88e3e3e8dd6ea482af5afd0a66
akshita1904 <parilovespakhi@gmail.com>
2021-04-04 16:33:30 +0530

    Pattern

---
+++ PatternV.java
@@ -0,0 +1,20 @@
+class PatternV
+{
+  public static void main(String args[])
+  {
+    for(int row = 1; row <= 5; row++)
+    {
+      for(int col = 1; col <= row; col++)
+      {
+        if(col%2==0){
+            System.out.print("#");
+        }
+        else{
+            System.out.print("$");
+        }
+      }
+      System.out.println();
+    }
+
+  }
+}
